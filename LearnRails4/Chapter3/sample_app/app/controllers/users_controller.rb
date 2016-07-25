e0ff06e0af6a19eecd42339369d9e5c5954d0b4f
MinhDuc <duc11t3bk@gmail.com>
2016-07-25 23:27:59 +0700

    chapter 9 finish

--- LearnRails4/Chapter3/sample_app/app/controllers/users_controller.rb
+++ LearnRails4/Chapter3/sample_app/app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-
+  before_action :admin_user, only: :destroy
   def index
-    @users= User.all
+    @users= User.paginate(page: params[:page])
   end
 
   def new
@@ -38,21 +38,31 @@ class UsersController < ApplicationController
     end
   end
 
-  private def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success]= "User deleted"
+    redirect_to users_url
   end
 
-  def logged_in_user
-    if !logged_in? 
-      store_location
-      flash[:danger]= "Please log in"
-      redirect_to login_url
+  private
+    def user_params
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
 
-  def correct_user
-    @user= User.find(params[:id])
-    redirect_to(root_url) unless @user== current_user
-  end
+    def logged_in_user
+      if !logged_in? 
+        store_location
+        flash[:danger]= "Please log in"
+        redirect_to login_url
+      end
+    end
 
+    def correct_user
+      @user= User.find(params[:id])
+      redirect_to(root_url) unless @user== current_user
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
