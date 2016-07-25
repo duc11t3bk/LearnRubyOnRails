10100f6ee9c47bf73b269184919204518b88e78d
MinhDuc <duc11t3bk@gmail.com>
2016-07-25 13:07:54 +0700

    finish chapter 8

--- LearnRails4/Chapter3/sample_app/test/test_helper.rb
+++ LearnRails4/Chapter3/sample_app/test/test_helper.rb
@@ -7,4 +7,24 @@ class ActiveSupport::TestCase
   fixtures :all
 
   include ApplicationHelper
+
+  def is_logged_in?
+  	return !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options={})
+  	password = options[:password] || "password"
+  	remember_me = options[:remember_me] || "1"
+  	if integration_test?
+  		post login_path, session: { email: user.email, password: password,
+  									remember_me: remember_me
+  		}
+  	else
+  		session[:user_id] = user_id
+  	end
+  end
+
+  def integration_test?
+  		defined?(post_via_redirect)
+  end
 end
