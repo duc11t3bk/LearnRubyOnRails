9ac831e3f587f9769891c2ce42af357852f8da03
MinhDuc <duc11t3bk@gmail.com>
2016-07-27 12:32:17 +0700

    Chapter 11 micropost

--- LearnRails4/Chapter3/sample_app/db/seeds.rb
+++ LearnRails4/Chapter3/sample_app/db/seeds.rb
@@ -16,4 +16,11 @@ User.create!(name: "Exmaple user",
 				 password_confirmation: password,
 				 activated: true,
 				 activated_at: Time.zone.now )
+
+users = User.order(:created_at).take(6)
+50.times do
+	content = Faker::Lorem.sentence(5)
+	users.each { |user| user.microposts.create!(content: content)}
+end
+
 end
